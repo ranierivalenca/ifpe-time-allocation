ce46664c6c1bc08ed543e721d91cd6144a5f56f1
Ranieri Valença <ranierivalenca@gmail.com>
2021-03-07 12:20:27 -0300

    Basic data

---
+++ classes.js
@@ -0,0 +1,289 @@
+module.exports = {
+    "ipi-1-t": {
+        "course_code": "ipi",
+        "semester": "ipi-1",
+        "shift": "tarde",
+        "teachers": {
+            "matematica": ["cicero"],
+            "logica": ["allan"],
+            "proif-inf": ["ranieri"],
+            "redes": ["lincoln"],
+            "fundamentos": ["liliane"],
+            "seguranca-trabalho":["danniel"],
+            "portugues": ["jefferson"],
+            "ingles": ["ivo"],
+            // "proif-port": [],
+            // "proif-mat": [],
+        }
+    },
+    "ipi-2-m": {
+        "course_code": "ipi",
+        "semester": "ipi-2",
+        "shift": "manha",
+        "teachers": {
+            "seguranca": ["ramon"],
+            "PeP 1": ["liliane"],
+            "BD": ["milton"],
+            "POO": ["milton"],
+            "web 1": ["ranieri"],
+            "SO": ["lincoln"],
+            "etica": ["willyans"],
+        }
+    },
+    "ipi-3-t": {
+        "course_code": "ipi",
+        "semester": "ipi-3",
+        "shift": "tarde",
+        "teachers": {
+            "IASW": ["ramon"],
+            "web 2": ["ranieri"],
+            "ES": ["milton"],
+            "PeP 2": ["ranieri","liliane"],
+            "IHC": ["liliane"],
+            "empreendedorismo": ["yuri"]
+        }
+    },
+
+
+
+
+
+
+
+    "tsi-1-m": {
+        "course_code": "tsi",
+        "semester": "tsi-1",
+        "shift": "manha",
+        "teachers": {
+            "programacao": ["allan"],
+            "fundamentos": ["ranieri"],
+            "redes": ["ramon"],
+            "design": ["elton"],
+            "intr-adm": ["yuri"],
+            "etica": ["willyans"],
+            "ingles-i": ["ivo"],
+            "calculo": ["mauricio"],
+        }
+    },
+    "tsi-2-m": {
+        "course_code": "tsi",
+        "semester": "tsi-2",
+        "shift": "manha",
+        "teachers": {
+            "web-1": ["ranieri"],
+            "bd": ["milton"],
+            // "isr": [],
+            // "projeto-1": [],
+            // "design-interface": [],
+            // "ppis": [],
+            // "ingles-ii": [],
+            // "estatistica": [],
+        }
+    },
+    "tsi-3-m": {
+        "course_code": "tsi",
+        "semester": "tsi-3",
+        "shift": "manha",
+        "teachers": {
+            "web-2": ["ranieri"],
+            "poo": ["milton"],
+            "seguranca": ["ramon"],
+            "design-interacao": ["elton"],
+            "pesq-an-comp": ["willyans"],
+            "ingles-iii": ["ivo"],
+            "metodologia": ["allan"],
+            "projeto-2": ["liliane"]
+        }
+    },
+
+
+
+
+
+
+
+
+
+
+    "log-1-m": {
+        "course_code": "log",
+        "semester": "log-1",
+        "shift": "manha",
+        "teachers": {
+            "informática": ["ramon"],
+            "int-logística": ["alexandre-m"],
+            "ética": ["willyans"],
+            "log-reversa": ["bruno"],
+            "intr-adm": ["alexandre-m"],
+            "matemática": ["cicero"],
+            "metodologia": ["danniel"],
+            "português": ["jefferson"],
+            // "proifpe-mat": [],
+            // "proifpe-port": [],
+        }
+    },
+    "log-1-t": {
+        "course_code": "log",
+        "semester": "log-1",
+        "shift": "tarde",
+        "teachers": {
+            "informática": ["liliane"],
+            "int-logística": ["alexandre-m"],
+            "ética": ["willyans"],
+            "log-reversa": ["bruno"],
+            "intr-adm": ["alexandre-m"],
+            "matemática": ["cicero"],
+            "metodologia": ["danniel"],
+            "português": ["jefferson"],
+            // "proifpe-mat": [],
+            // "proifpe-port": [],
+        }
+    },
+    "log-2-t": {
+        "course_code": "log",
+        "semester": "log-2",
+        "shift": "tarde",
+        "teachers": {
+            "com-e-rel-intern": ["tarcisio"],
+            "leg-e-trib": ["michelle"],
+            "log-de-armazenagem": ["tarcisio"],
+            "gestão-de-pessoas": ["yuri"],
+            "log-transp-dist": ["alexandre-m"],
+            "gerenc-e-economia": ["luiz-guim"],
+            "estatística": ["danniel"],
+            "inglês": ["ivo"],
+        }
+    },
+    "log-3-t": {
+        "course_code": "log",
+        "semester": "log-3",
+        "shift": "tarde",
+        "teachers": {
+            "segurança": ["danniel"],
+            "tecn-e-si-log": ["alexandre-m"],
+            "custos-logísticos": ["tarcisio"],
+            "gestão-da-qualidade": ["francisco"],
+            "gestão-de-materiais": ["inez"],
+            "gestão-da-produção": ["djalma"],
+            "inglês-ii": ["ivo"],
+            "scm": ["yuri"],
+            "tópicos-especiais": ["tarcisio"],
+        }
+    },
+    "log-3-n": {
+        "course_code": "log",
+        "semester": "log-3",
+        "shift": "noite",
+        "teachers": {
+            "segurança": ["danniel"],
+            "tecn-e-si-log": ["alexandre-m"],
+            "custos-logísticos": ["tarcisio"],
+            "gestão-da-qualidade": ["francisco"],
+            "gestão-de-materiais": ["inez"],
+            "gestão-da-produção": ["djalma"],
+            "inglês-ii": ["ivo"],
+            "scm": ["yuri"],
+            "tópicos-especiais": ["tarcisio"],
+        }
+    },
+
+
+
+
+
+
+
+
+
+    "adm-1-m": {
+        "course_code": "adm",
+        "semester": "adm-1",
+        "shift": "manha",
+        "teachers": {
+            "com-empr": ["renata"],
+            "etica": ["willyans"],
+            "intr-adm": ["luiz-guim"],
+            "intr-cont": ["michelle"],
+            "intr-economia": ["luiz-guim"],
+            "matematica": ["mauricio"],
+        }
+    },
+    "adm-3-m": {
+        "course_code": "adm",
+        "semester": "adm-3",
+        "shift": "manha",
+        "teachers": {
+            "gestao-pessoas-i": ["renata"],
+            "comportamento": ["willyans"],
+            "direito": ["michelle"],
+            "adm-leg-trib": ["michelle"],
+            "metodologia": ["danniel"],
+            "organizacao": ["djalma"],
+        }
+    },
+
+
+
+
+
+
+
+
+
+    "tgq-1-m": {
+        "course_code": "tgq",
+        "semester": "tgq-1",
+        "shift": "manha",
+        "teachers": {
+            "fund-de-economia": ["luiz-guim"],
+            "fundamentos-adm": ["yuri"],
+            "intr-a-gest-qual": ["djalma"],
+            "matemática": ["mauricio"],
+            "português": ["jefferson"],
+            "ética": ["willyans"],
+            "informática": ["liliane"],
+        }
+    },
+    "tgq-3-m": {
+        "course_code": "tgq",
+        "semester": "tgq-3",
+        "shift": "manha",
+        "teachers": {
+            "proj-integr-ii": ["inez"],
+            "estastística-ii": ["djalma"],
+            "gest-des-prod": ["breno"],
+            "gest-processo": ["francisco"],
+            "tsi-apl": ["lincoln"],
+            "gest-qual-serv": ["luiz-guim"],
+            "gest-prod": ["djalma"],
+        }
+    },
+    "tgq-4-t": {
+        "course_code": "tgq",
+        "semester": "tgq-4",
+        "shift": "tarde",
+        "teachers": {
+            "gest-estr-quali": ["breno"],
+            "gest-inovacao": ["yuri"],
+            "norm-cert-qual": ["djalma"],
+            "metrologia": ["francisco"],
+            "custos-prod": ["michelle"],
+            "cont-est-quali": ["luiz-guim"],
+            "metodologia": ["jefferson"],
+            "planej-estrat": ["renata"],
+        }
+    },
+    "tgq-5-m": {
+        "course_code": "tgq",
+        "semester": "tgq-5",
+        "shift": "manha",
+        "teachers": {
+            "ergonomia": ["danniel"],
+            "gestao-projetos": ["breno"],
+            "metodologia-ii": ["inez"],
+            "empreendedorismo": ["yuri"],
+            "auditoria": ["djalma"],
+            "direito": ["francisco"],
+        }
+    }
+}
---
+++ courses.js
@@ -0,0 +1,190 @@
+module.exports = {
+    "ipi": {
+        "name": "Informática para Internet",
+        "class_time": 45,
+        "semesters": {
+            "ipi-1": {
+                "matematica": [2, 2],
+                "logica": [2, 3],
+                "proif-inf": [1],
+                "redes": [2, 2],
+                "fundamentos": [3],
+                "seguranca-trabalho": [2],
+                "portugues": [3],
+                "ingles": [2],
+                "proif-port": [2],
+                "proif-mat": [2],
+            },
+            "ipi-2": {
+                "seguranca": [3],
+                "PeP 1": [4],
+                "BD": [2, 2],
+                "POO": [3],
+                "web 1": [2, 2],
+                "SO": [3],
+                "etica": [2],
+            },
+            "ipi-3": {
+                "IASW": [2, 3],
+                "web 2": [4],
+                "ES": [2, 2],
+                "PeP 2": [2, 3],
+                "IHC": [3],
+                "empreendedorismo": [2],
+            }
+        }
+    },
+    "log": {
+        "name": "Técnico em Logística",
+        "class_time": 45,
+        "semesters": {
+            "log-1": {
+                "informática": [3],
+                "int-logística": [3],
+                "intr-adm": [3],
+                "log-reversa": [3],
+                "matemática": [2],
+                "metodologia": [2],
+                "português": [3],
+                "proifpe-mat": [1],
+                "proifpe-port": [2],
+                "ética": [2],
+            },
+            "log-2": {
+                "com-e-rel-intern": [3],
+                "estatística": [3],
+                "gerenc-e-economia": [2],
+                "gestão-de-pessoas": [3],
+                "inglês": [2],
+                "leg-e-trib": [2],
+                "log-de-armazenagem": [3],
+                "log-transp-dist": [3],
+            },
+            "log-3": {
+                "custos-logísticos": [2],
+                "gestão-da-produção": [3],
+                "gestão-da-qualidade": [3],
+                "gestão-de-materiais": [3],
+                "inglês-ii": [2],
+                "scm": [3],
+                "segurança": [2],
+                "tecn-e-si-log": [2],
+                "tópicos-especiais": [3],
+            }
+        }
+    },
+    "tsi": {
+        "name": "Tecnólogo em Sistemas para Internet",
+        "class_time": 45,
+        "semesters": {
+            "tsi-1": {
+                "programacao": [2, 2],
+                "fundamentos": [2],
+                "redes": [3, 3],
+                "design": [3],
+                "intr-adm": [3],
+                "etica": [3],
+                "ingles-i": [3],
+                "calculo": [3],
+            },
+            "tsi-2": {
+                "web-1": [2, 2],
+                "bd": [2, 2],
+                "isr": [2, 2],
+                "projeto-1": [2],
+                "design-interface": [2, 2],
+                "ppis": [3],
+                "ingles-ii": [3],
+                "estatistica": [3]
+            },
+            "tsi-3": {
+                "web-2": [2, 2],
+                "poo": [2, 2],
+                "seguranca": [2, 2],
+                "design-interacao": [3],
+                "pesq-an-comp": [3],
+                "ingles-iii": [3],
+                "metodologia": [2],
+                "projeto-2": [2]
+            }
+        }
+    },
+    "tgq": {
+        "name": "Tecnólogo em Gestão da Qualidade",
+        "class_time": 45,
+        "semesters": {
+            "tgq-1": {
+                "fund-de-economia": [3],
+                "fundamentos-adm": [3],
+                "informática": [3],
+                "intr-a-gest-qual": [3],
+                "matemática": [3],
+                "português": [3],
+                "ética": [2],
+            },
+            "tgq-2": {
+                "eng-econômica": [3],
+                "estatística-i": [3],
+                "fer-qualidade": [3],
+                "gestão-amb": [2],
+                "gestão-de-materiais": [3],
+                "gestão-de-pessoas": [3],
+                "metodologia": [3],
+                "proj-integardor": [3],
+            },
+            "tgq-3": {
+                "estastística-ii": [3],
+                "gest-des-prod": [3],
+                "gest-processo": [3],
+                "gest-prod": [3],
+                "gest-qual-serv": [3],
+                "proj-integr-ii": [3],
+                "tsi-apl": [3],
+            },
+            "tgq-4": {
+                "gest-estr-quali": [3],
+                "norm-cert-qual": [3],
+                "metrologia": [2, 2],
+                "custos-prod": [2],
+                "planej-estrat": [2],
+                "cont-est-quali": [3],
+                "metodologia": [2],
+                "gest-inovacao": [3]
+            },
+            "tgq-5": {
+                "ergonomia": [3],
+                "gestao-projetos": [3],
+                "auditoria": [3],
+                "direito": [3],
+                "empreendedorismo": [3],
+                "metodologia-ii": [2],
+            }
+        }
+    },
+    "adm": {
+        "name": "Bacharelado em Administração",
+        "class_time": 60,
+        "semesters": {
+            "adm-1": {
+                "intr-adm": [3],
+                "intr-economia": [3],
+                "matematica": [3],
+                "intr-cont": [3],
+                "com-empr": [2],
+                "etica": [2],
+            },
+            "adm-2": {
+
+            },
+            "adm-3": {
+               "comportamento": [3],
+               "direito": [3],
+               "gestao-pessoas-i": [3],
+               "adm-leg-trib": [2],
+               "metodologia": [3],
+               "organizacao": [2],
+            }
+        }
+    }
+};
+
